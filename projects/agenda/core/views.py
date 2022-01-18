3059f42303e42a681e5dc57f0ed1ed48ef8e3bd7
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2022-01-17 21:07:25 -0300

    Add rota que mostra Evento
    Add rota que mostra Evento

--- projects/agenda/core/views.py
+++ projects/agenda/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+
+def events(request, titulo_evento):
+    titulo = Evento.objects.get(titulo=titulo_evento)
+    return HttpResponse(f'<h1>{titulo}</h1>')
