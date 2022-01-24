e52cc26380be212b0f5de424938151f5011e06e4
Renato Magalhães <rrmagalhaes@hotmail.com.br>
2022-01-24 19:55:16 -0300

    Adiciona tela de inserção de eventos
    Adiciona tela de inserção de eventos

--- projects/agenda/core/models.py
+++ projects/agenda/core/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# python manage.py makemigrations core (cria arquivo com migrações)
+# python manage.py sqlmigrate core (mostra sql que será aplicado)
+# python manage.py migrate core XXXX (aplica alterações no banco)
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True,verbose_name='Data de Criação')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         db_table = 'events'
--- projects/agenda/core/views.py
+++ projects/agenda/core/views.py
@@ -34,3 +34,23 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = { 'eventos': evento }
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        local = request.POST.get('local')
+        data_evento = request.POST.get('data_evento')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              descricao=descricao,
+                              data_evento=data_evento,
+                              usuario=usuario,
+                              local=local)
+    return redirect("/")
+
